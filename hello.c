e5e3db3122f2bead7769bebe190514a7481bd513
รัฐศาสตร์ ทุ่งสิบสาม <ratthasat9826@gmail.com>
2026-07-02 16:19:20 +0700

    Add VSCode configuration files and initial C program
    - Created c_cpp_properties.json for C/C++ IntelliSense configuration.
    - Added launch.json for debugging configurations.
    - Introduced settings.json for terminal and file associations.
    - Implemented tasks.json for build tasks with MSYS2 and Raylib.
    - Added initial hello.c program with basic output.
    - Included 03-Setup-VSCode.ps1 for setup automation.

---
+++ hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    printf("สวัสดีครับ ผม รัฐศาสตร์ ทุ่งสิบสาม\n");
+    printf("รหัสนักศึกษา: 692110166\n");
+    printf("วิชาDG111 Computer Programming\n");
+    return 0;
+}
